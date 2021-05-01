35b9e37a13c193920e3fb7135930f534d792cf80
Adam Landriault <>
2021-05-01 21:25:49 +0000

    fixed errors

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function myFunction() {
-  document.getElementById("demo").style.fontSize = "25px"; 
-  document.getElementById("demo").style.color = "red";
-  document.getElementById("demo").style.backgroundColor = "yellow";      
-  document.getElementById("myImg").width = "300";  
+ function myFunction() {
+  document.getElementById('demo').style.fontSize = '25px'
+  document.getElementById('demo').style.color = 'red'
+  document.getElementById('demo').style.backgroundColor = "yellow";
+  document.getElementById('myImg').width = "300"
 }
-
