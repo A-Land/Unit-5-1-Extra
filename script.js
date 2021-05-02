a882fcd2c1d6593f8b5cd1b274cf9c8daf364e37
Adam Landriault <>
2021-05-02 23:14:30 +0000

    fixed errors

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('demo').style.textAlign = 'center'
 document.getElementById('myh2').style.color = 'blue'
-function myFunction () {
+function myFunction(p1) {
   const x = document.createElement('IMG')
   x.setAttribute('src', 'HW.png')
   x.setAttribute('width', '304')
