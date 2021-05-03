9da1d6f6f440e011dbc509758dba610834a865a5
Adam Landriault <>
2021-05-03 15:38:46 +0000

    fixed errors again

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById('demo').style.textAlign = 'center'
 document.getElementById('myh2').style.color = 'blue'
-document.getElementById('image').addEventListener ('click', myImage)
-function myImage (){
+document.getElementById('image').addEventListener('click', myImage)
+function myImage(){
   document.getElementById('img').style.display = 'block'
 }
 const x = document.createElement('IMG')
-  x.setAttribute('src', 'HW.png')
-  x.setAttribute('width', '304')
-  x.setAttribute('height', '228')
-  x.setAttribute('alt', 'Hello World!')
-  document.body.appendChild(x)
+x.setAttribute('src', 'HW.png')
+x.setAttribute('width', '304')
+x.setAttribute('height', '228')
+x.setAttribute('alt', 'Hello World!')
+document.body.appendChild(x)
